43182be15912a3e52207eb383ed716d7986c5710
binhnqq <128908540+binhnqq@users.noreply.github.com>
2023-07-16 23:07:14 +0700

    Add files via upload

---
+++ react-datve/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomeDatVe from "./DatveComponent/Homecomponent/HomeDatVe";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeDatVe />
+    </div>
+  );
+}
+
+export default App;
